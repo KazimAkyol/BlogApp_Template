84f01e26a8479f3b7fe23c4c25200376313ce620
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-14 13:01:42 +0200

    divide index.ejs

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
     EXPRESSJS - EJS-BLOG Project with Mongoose
 ------------------------------------------------------- */
 
+// $ npm init -y
+// $ npm i express dotenv express-async-errors
+// $ echo PORT=8000 > .env
+// $ nodemon
+
 const express = require("express");
 const app = express();
 
@@ -10,13 +15,36 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 const session = require("cookie-session");
+const { openDelimiter, closeDelimiter } = require("ejs");
 app.use(
-  session({ secret: process.env.SECRET_KEY || "secret_keys_for_cookies" }),
+  session({ secret: process.env.SECRET_KEY || "secret_keys_for_cookies" })
 );
 /* ------------------------------------------------------- */
 // Accept json data & convert to object:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+/* ------------------------------------------------------- */
+// EJS
+
+// <% %> isaretlerine delimiter denir.
+// Default delimiter <% %>
+// {%   %}
+
+//* Default delimiter degistirmek icin 1.Yöntem:
+// require("ejs");
+// ejs.delimiter = "#";
+// ejs.openDelimiter = "{"; // < yerine { kullanmak icin
+// ejs.closeDelimiter = "}"; // > yerine } kullanmak icin
+
+//* 2.Yöntem: //* daha yaygin kullanilir.
+app.set("view engine", "ejs");
+app.set("view options", {
+  openDelimiter: "{",
+  closeDelimiter: "}",
+});
+
+//* Default olarak views yerine public klasörünü tanimliyoruz:
+app.set("views", "./public");
 
 // Connect to MongoDB with Mongoose:
 require("./src/dbConnection");
@@ -45,4 +73,4 @@ app.use(require("./src/middlewares/errorHandler"));
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
 //require('./src/helpers/sync')()
-//require("./src/helpers/sync2")();
+// require("./src/helpers/sync2")();
